f5164b42e3ef6a0de08e68992d213ffc19206a66
TJ DeVries <devries.timothyj@gmail.com>
2022-02-10 15:48:36 -0500

    update gofmt to ignore embedded test file code for tree sitter

--- dev/check/gofmt.sh
+++ dev/check/gofmt.sh
@@ -8,7 +8,15 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 
 # Check if all code is gofmt'd
 
-DIFF=$(find . \( -path ./vendor -o -path ./vendored \) -prune -o -name '*.go' -exec gofmt -s -w -d {} +)
+# IGNORE:
+#   vendored: Vendored code doesn't matter for gofmt
+#   syntax-highlighter/crates: has embedded go test files
+
+DIFF=$(find . \( \
+    -path ./vendor \
+    -o -path ./vendored \
+    -o -path ./docker-images/syntax-highlighter/crates \
+    \) -prune -o -name '*.go' -exec gofmt -s -w -d {} +)
 if [ -z "$DIFF" ]; then
   echo "Success: gofmt check passed."
   exit 0
